d306152e43ebd0bd46d58af2ca93c4622541184f
Daniil Zaytsev <daniil_zaytsev_02@mail.ru>
2023-09-18 09:51:20 +0300

    final step

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Items from "./Components/Items";
+import Categories from "./Components/Categories";
+import ShowFullItem from "./Components/ShowFullItem";
 
 
 class App extends React.Component{
@@ -9,6 +11,7 @@ class App extends React.Component{
     super(props);
     this.state = {
       orders: [],
+      currentItems: [],
       items: [
         {
           id: 1,
@@ -34,19 +37,42 @@ class App extends React.Component{
           category: 'bed',
           price: '189.99'
         }
-      ]
+      ],
+      showFullItem: false,
+      fullItem: {}
     }
+    this.state.currentItems = this.state.items
     this.addToOder = this.addToOder.bind(this)
     this.deleteOrder = this.deleteOrder.bind(this)
+    this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
   }
   render() {
     return (
       <div className="wrapper">
         <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
-        <Items items={this.state.items} onAdd={this.addToOder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOder} />
+        {this.state.showFullItem && <ShowFullItem onAdd={this.addToOder} onShowItem={this.onShowItem} item={this.state.fullItem} />}
         <Footer />
       </div>
     );
+  }
+  
+  onShowItem(item) {
+    this.setState({fullItem: item})
+    this.setState({showFullItem: !this.state.showFullItem})
+  }
+
+  chooseCategory(category) {
+    if(category === 'all') {
+      this.setState({currentItems: this.state.items})
+      return
+    }
+    
+    this.setState({
+      currentItems: this.state.items.filter(el => el.category === category)
+    })
   }
 
   deleteOrder(id) {
